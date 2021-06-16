44a6cec8176c83927254a09531e40079982fed74
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2021-06-16 18:53:21 +0900

    tests

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from selenium import webdriver
 import json
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # 定数
 URL_WACCA = "https://wacca.marv.jp/music/"
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(executable_path="./geckodriver.exe")
 
 browser.get(URL_WACCA)
+browser.find_element(By.XPATH, '//a[@class="genre" and text()="すべて"]').click()
+browser.quit()
