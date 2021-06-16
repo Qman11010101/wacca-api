8e8279294ebd331404ec40cb02ddc80523bb70c5
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-06-17 00:36:12 +0900

    完成

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
 import json
+import time
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 # 定数
+with open("pathSetting.json", encoding="utf-8_sig") as j:
+    PATHJSON = json.load(j)
 URL_WACCA = "https://wacca.marv.jp/music/"
-browser = webdriver.Firefox(executable_path="./geckodriver.exe")
+API_PATH = PATHJSON["export_path"] + "wacca_all.json"
 
-browser.get(URL_WACCA)
-browser.find_element(By.XPATH, '//a[@class="genre" and text()="すべて"]').click()
-browser.quit()
+options = Options()
+options.add_argument('--headless') # ヘッドレス
+driver = webdriver.Firefox(options=options)
+
+driver.get(URL_WACCA)
+driver.find_element(By.XPATH, '//a[@class="genre" and text()="すべて"]').click()
+time.sleep(1)
+source = driver.page_source
+driver.quit()
+
+html = BeautifulSoup(source, "html.parser")
+musics = [m for m in html.find_all("div", {"class": "song"})]
+
+music_json = []
+for m in musics:
+    title = m.find_all("div", {"class": "data_name"})[0].get_text()
+    artist = m.find_all("dd")[0].get_text()
+    category = m.find_all("div", {"class": "data_cat"})[0].get_text()
+    lvs = m.find_all("div", {"class": "level_value"})
+    has_inf = True if len(lvs) == 4 else False
+    lv_nor = lvs[0].get_text().strip()
+    lv_har = lvs[1].get_text().strip()
+    lv_exp = lvs[2].get_text().strip()
+    lv_inf = lvs[3].get_text().strip() if has_inf else None
+    tempobj = {
+        "meta": {
+            "title": title,
+            "artist": artist,
+            "category": category
+        },
+        "level": {
+            "nor": lv_nor,
+            "har": lv_har,
+            "exp": lv_exp
+        }
+    }
+    if has_inf:
+        tempobj["level"]["inf"] = lv_inf
+    music_json.append(tempobj)
+
+with open(API_PATH, "w", encoding="utf-8_sig") as api:
+    json.dump(music_json, api, ensure_ascii=False)
