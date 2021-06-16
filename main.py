92893bd28dc2f01b9d81f680f4d5a21e1b620fdb
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2021-06-17 01:45:53 +0900

    log削除+取れるまでやり直し

--- main.py
+++ main.py
@@ -11,24 +11,25 @@ with open("pathSetting.json", encoding="utf-8_sig") as j:
     PATHJSON = json.load(j)
 URL_WACCA = "https://wacca.marv.jp/music/"
 API_PATH = PATHJSON["export_path"] + "wacca_all.json"
-print("Path Loaded:" + API_PATH)
 
 options = Options()
 options.add_argument('--headless') # ヘッドレス
 driver = webdriver.Firefox(options=options, executable_path="./geckodriver")
-print("Firefox launched")
 
 driver.get(URL_WACCA)
-driver.find_element(By.XPATH, '//a[@class="genre" and text()="すべて"]').click()
-print("Page moved. Interval 15sec start")
-time.sleep(15)
-source = driver.page_source
-driver.quit()
-print("Firefox terminated")
 
-html = BeautifulSoup(source, "html.parser")
-musics = [m for m in html.find_all("div", {"class": "song"})]
-print("HTML Loaded")
+ms_count_check = 0
+wait_time = 10
+
+while ms_count_check < 100:
+    driver.find_element(By.XPATH, '//a[@class="genre" and text()="すべて"]').click()
+    time.sleep(wait_time)
+    source = driver.page_source
+
+    html = BeautifulSoup(source, "html.parser")
+    musics = [m for m in html.find_all("div", {"class": "song"})]
+    ms_count_check = len(musics)
+    wait_time += 5
 
 music_json = []
 for m in musics:
@@ -56,8 +57,6 @@ for m in musics:
     if has_inf:
         tempobj["level"]["inf"] = lv_inf
     music_json.append(tempobj)
-    print(f"Wrote to tempobj: {title}")
 
 with open(API_PATH, "w", encoding="utf-8_sig") as api:
     json.dump(music_json, api, ensure_ascii=False)
-print("JSON Dumped!")
